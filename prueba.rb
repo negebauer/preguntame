c9fb647a0faee516c5048b39de090a61043d62c3
rtacuna <rtacuna@uc.cl>
2016-06-29 16:17:42 -0400

    conectandose a la api de twitter

--- prueba.rb
+++ prueba.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'twitter'
 
-client = Twitter::Streaming::Client.new do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "jG4IFhzllIJCJDCKEoY8s1IFI"
   config.consumer_secret     = "1hsqWBEWOQLwk7v708PYIstw6DgJE9wFVlYcENXW5nYnayULpw"
   config.access_token        = "109609974-QonFgD89kFYEFfHF8mUwml7xXlCsuUrAJhZSyHRq"
   config.access_token_secret = "eTAxguM6b0IZV0yOJ6VF7K7V6ocygs6GmGWtpdIBEIhiT"
 end
 
-client.sample do |object|
-  puts object.text if object.is_a?(Twitter::Tweet)
+client.search("to:Chile", result_type: "recent").take(100).collect do |tweet|
+  puts"#{tweet.user.screen_name}: #{tweet.text}"
 end
