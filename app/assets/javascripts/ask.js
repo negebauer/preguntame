4fae58c9e0e75ba9bfd9103e4814d5157f3e3d6b
negebauer <negebauer@uc.cl>
2016-06-29 18:20:22 -0400

    borrar mensaje de error. mostrar error ejecucion pregunta.

--- app/assets/javascripts/ask.js
+++ app/assets/javascripts/ask.js
@@ -4,11 +4,10 @@ $(function() {
 
 function makeQuestion(ev) {
   // Bloquear post del formulario
-  ev.preventDefault();
+  ev.preventDefault()
   // Borrar info anterior
-  $('.error').html('')
-  $('.response').html('')
-  // Obtenemos la pregunta
+  cleanResponse()
+    // Obtenemos la pregunta
   var question = $('textarea.question').val()
   var error = isQuestionValid(question)
   if (error) {
@@ -16,6 +15,11 @@ function makeQuestion(ev) {
   } else {
     questionExecute(question)
   }
+}
+
+function cleanResponse() {
+  $('.error').html('')
+  $('.response').html('')
 }
 
 function isQuestionValid(question) {
@@ -28,14 +32,17 @@ function questionExecute(question) {
   $.ajax({
     type: "POST",
     url: "/ask/respond",
-    data: {"question": question},
+    data: {
+      "question": question
+    },
     success: function(result) {
       var html = '<h3>Respuesta</h3><p>' + result + '</p>'
       $('.response').html(html)
     },
     error: function(req, status, error) {
-      var html = '<div class="alert"><span class="closebtn">&times;</span><h4>La pregunta ingresada es inválida, intenta con otra pregunta...</h4>  </div>'
+      var html = '<div class="alert"><span class="closebtn">&times;</span><h4>La pregunta ingresada es inválida, intenta con otra pregunta...</h4>' + error + '</div>'
       $('.error').html(html)
+      $('.closebtn').on('click', cleanResponse)
     }
   });
 }
@@ -43,17 +50,5 @@ function questionExecute(question) {
 function questionError(error) {
   var html = '<div class="alert"><span class="closebtn">&times;</span><h4>La pregunta ingresada es inválida, intenta con otra pregunta...</h4>  </div>'
   $('.error').html(html)
-}
-
-function deletebar(error){
-var close = document.getElementsByClassName("closebtn");
-var i;
-
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.opacity = "0";
-        setTimeout(function(){ div.style.display = "none"; }, 600);
-    }
-}
+  $('.closebtn').on('click', cleanResponse)
 }
