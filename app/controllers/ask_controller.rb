7f6db967b4adcf12ea93fd3c0c365ec806f407da
rtacuna <rtacuna@uc.cl>
2016-06-29 17:19:46 -0400

    stop words

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -1,4 +1,8 @@
 class AskController < ApplicationController
+  before_action :stop_words_check
+  before_action :stop_words_load
+
+  @@stop_words = []
 
   def home
 
@@ -8,12 +12,33 @@ class AskController < ApplicationController
     # HACER REQUEST A LA WEA
     # USAR JAVASCRIPT PARA MOSTRAR RESPUESTA
     question = params.require(:question)
+    filter = Stopwords::Filter.new @@stop_words
+    data = filter.filter question.split
+
     puts '----- LEEME -----'
     puts question
     respond_to do |format|
         format.html { render body: 'Respuesta para "' + question + '"' }
         format.js {'js'}
         format.json { 'json' }
+    end
+  end
+
+  private
+
+  def stop_words_check
+    if @@stop_words.empty?
+      stop_words_read
+    end
+  end
+
+  def stop_words_load
+    @stop_words = @@stop_words
+  end
+
+  def stop_words_read
+    File.open("stopwords_es-2.txt", "r").each_line do |line|
+      @@stop_words << line.strip
     end
   end
 end
