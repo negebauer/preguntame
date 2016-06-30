bc6975e94388fc05eb23e36269920179e94d2a8c
rtacuna <rtacuna@uc.cl>
2016-06-29 23:47:45 -0400

    estadisticas json

--- prueba.rb
+++ prueba.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'twitter'
-require 'unirest'
+#require 'unirest'
 
-require 'active_support/inflector'
+#require 'active_support/inflector'
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "jG4IFhzllIJCJDCKEoY8s1IFI"
@@ -11,20 +11,8 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = "eTAxguM6b0IZV0yOJ6VF7K7V6ocygs6GmGWtpdIBEIhiT"
 end
 
-message = ""
+
 client.search("eurocopa", result_type: "today").take(1).collect do |tweet|
-  message += tweet.full_text+"\n"
+  puts tweet.full_text
 
 end
-
-x = ActiveSupport::Inflector.transliterate(message).to_s
-response = Unirest.post "https://api.meaningcloud.com/sentiment-2.1",
-parameters:{
-  key: "68e8c30899c70cee783b176a3c6eb140",
-  lang: "es",
-  txt: x}
-
-
-#message = I18n.transliterate(message)
-#puts message
-puts response.raw_body
--- prueba2.rb
+++ prueba2.rb
@@ -12,6 +12,30 @@
 
 require 'uri'
 require 'net/http'
+require 'json'
+require 'twitter'
+
+mensaje = ""
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "jG4IFhzllIJCJDCKEoY8s1IFI"
+  config.consumer_secret     = "1hsqWBEWOQLwk7v708PYIstw6DgJE9wFVlYcENXW5nYnayULpw"
+  config.access_token        = "109609974-LgXglwk0qrQGSM0kAlfsXFQntmr6vPtBKkPTFIgY"
+  config.access_token_secret = "gYWOfI2n7YJrv9wdLmx1aeO9kajtSl93IgMM2muu0GMmE"
+end
+
+
+client.search("eurocopa", result_type: "today").take(50).collect do |tweet|
+  texto = tweet.full_text.split(" ")
+  texto.each do |palabra|
+    if palabra.include? 'https'
+      puts palabra + ' BLABLABLAL'
+      texto.delete(palabra)
+    end
+  end
+  texto = texto.join(" ")
+  mensaje = mensaje + texto + "\n"
+end
+
 
 url = URI("http://api.meaningcloud.com/sentiment-2.1")
 
@@ -19,7 +43,60 @@ http = Net::HTTP.new(url.host, url.port)
 
 request = Net::HTTP::Post.new(url)
 request["content-type"] = 'application/x-www-form-urlencoded'
-request.body = "key=68e8c30899c70cee783b176a3c6eb140&lang=es&txt=Hola como estas"
+request.body = "key=68e8c30899c70cee783b176a3c6eb140&lang=es&txt=#{mensaje}"
 
 response = http.request(request)
-puts response.read_body
+#puts response.read_body
+data =  JSON.parse(response.body)
+
+
+pos = 0
+neu = 0
+neg = 0
+strongpos = 0
+strongneg = 0
+
+items = {}
+
+respuesta_general = data["score_tag"]
+seguridad = data["confidence"]
+data["sentence_list"].each do |tweet|
+  case tweet["score_tag"]
+  when "P"
+    pos += 1
+  when "P+"
+    strongpos += 1
+  when "NEU"
+    neu += 1
+  when "N"
+    neg +=1
+  when "N+"
+    strongneg += 1
+  end
+  #temas
+  tweet["segment_list"].each do |uno|
+    uno["polarity_term_list"].each do |dos|
+      if !dos["sentimented_concept_list"].nil?
+        dos["sentimented_concept_list"].each do |key|
+          if items.keys.include? key["form"]
+            items[key["form"]] +=1
+          else
+            items[key["form"]] = 1
+          end
+        end
+      end
+      if !dos["sentimented_entity_list"].nil?
+        dos["sentimented_entity_list"].each do |item|
+          if items.keys.include? item["form"]
+            items[item["form"]] +=1
+          else
+            items[item["form"]] = 1
+          end
+        end
+      end
+    end
+  end
+
+end
+
+puts items
